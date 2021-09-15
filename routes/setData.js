3b81e574525db304bfd29310369820d6fca87f36
ShavarshP <papoyan101097@mail.ru>
2021-09-15 23:38:15 +0400

    test: heroku add auth_middleware

--- routes/setData.js
+++ routes/setData.js
@@ -1,65 +1,49 @@
-// const { Router } = require("express");
-// const router = Router();
 const { Router } = require("express");
 const router = Router();
+
 // const Home = require("../models/Home");
 
-router.get("/partants", async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
-    res.json([
-      { name: "Armine", type: "gerazancik" },
-      { name: "Shavarsh", type: "maladec" },
-      { name: "Artyom", type: "shaxmat" },
-      { name: "Artur", type: "ft-t-t-t--t-t-t" },
-    ]);
+    const {
+      sale,
+      rent,
+      search_code,
+      category,
+      district,
+      building_type,
+      rooms,
+      price,
+      floor,
+      area,
+      street,
+      Mobile_number,
+      description,
+      img,
+    } = req.body;
+    // const home = new Home({
+    //   img: img,
+    //   Mobile_number: Mobile_number,
+    //   area: area,
+    //   building_type: building_type,
+    //   category: category,
+    //   street: street,
+    //   description: description,
+    //   district: district,
+    //   floor: floor,
+    //   price: price,
+    //   rent: rent,
+    //   rooms: rooms,
+    //   sale: sale,
+    //   search_code: search_code,
+    //   loc: "",
+    // });
+    // await home.save();
+    res.status(201).json({ message: "data created" });
   } catch (e) {
+    console.log(e);
     res.status(500).json({ message: "Something went wrong, please try again" });
   }
 });
 
 module.exports = router;
-
-// router.post("/add", async (req, res) => {
-//   try {
-//     const {
-//       sale,
-//       rent,
-//       search_code,
-//       category,
-//       district,
-//       building_type,
-//       rooms,
-//       price,
-//       floor,
-//       area,
-//       street,
-//       Mobile_number,
-//       description,
-//       img,
-//     } = req.body;
-//     const home = new Home({
-//       img: img,
-//       Mobile_number: Mobile_number,
-//       area: area,
-//       building_type: building_type,
-//       category: category,
-//       street: street,
-//       description: description,
-//       district: district,
-//       floor: floor,
-//       price: price,
-//       rent: rent,
-//       rooms: rooms,
-//       sale: sale,
-//       search_code: search_code,
-//       loc: "",
-//     });
-//     await home.save();
-//     res.status(201).json({ message: "Пользователь создан" });
-//   } catch (e) {
-//     console.log(e);
-//     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
-//   }
-// });
-
-// module.exports = router;
