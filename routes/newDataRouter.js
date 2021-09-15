2aca16157bf84c8b542f350fa0590e8a1e1186fb
ShavarshP <papoyan101097@mail.ru>
2021-09-15 23:12:03 +0400

    fix server

--- routes/newDataRouter.js
+++ routes/newDataRouter.js
@@ -4,19 +4,19 @@ const router = Router();
 
 const Home = require("../models/story");
 
-router.get("/houses", async (req, res) => {
-  const candidate = await Home.find().sort({ $natural: -1 }).limit(8);
-  res.json(candidate);
-});
+// router.get("/houses", async (req, res) => {
+//   const candidate = await Home.find().sort({ $natural: -1 }).limit(8);
+//   res.json(candidate);
+// });
 
-router.get("/myhome/:_id", async (req, res) => {
-  try {
-    const id = req.params;
-    const homedate = await Home.find(id);
-    res.json(homedate);
-  } catch (e) {
-    res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
-  }
-});
+// router.get("/myhome/:_id", async (req, res) => {
+//   try {
+//     const id = req.params;
+//     const homedate = await Home.find(id);
+//     res.json(homedate);
+//   } catch (e) {
+//     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
+//   }
+// });
 
 module.exports = router;
