390ecaddc181cbd8480128d3ab7d4da0a6607c87
ShavarshP <papoyan101097@mail.ru>
2021-09-16 16:33:00 +0400

    test: heroku

--- routes/createData.js
+++ routes/createData.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
 const router = Router();
-const Home = require("../models/story");
+// const Home = require("../models/story");
 
-router.get("/houses", async (req, res) => {
-  const candidate = await Home.find().sort({ $natural: -1 }).limit(8);
-  res.json(candidate);
-});
+// router.get("/houses", async (req, res) => {
+//   const candidate = await Home.find().sort({ $natural: -1 }).limit(8);
+//   res.json(candidate);
+// });
 
-router.get("/myhome/:_id", async (req, res) => {
-  try {
-    const id = req.params;
-    const homedate = await Home.find(id);
-    res.json(homedate);
-  } catch (e) {
-    res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
-  }
-});
+// router.get("/myhome/:_id", async (req, res) => {
+//   try {
+//     const id = req.params;
+//     const homedate = await Home.find(id);
+//     res.json(homedate);
+//   } catch (e) {
+//     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
+//   }
+// });
 
 module.exports = router;
