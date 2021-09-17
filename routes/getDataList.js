4ae5b95ab832aac172bd33364dc64138ce5a602d
ShavarshP <papoyan101097@mail.ru>
2021-09-17 13:04:18 +0400

    fix: bugs getData

--- routes/getDataList.js
+++ routes/getDataList.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 
 const router = Router();
-
 const createSbjectSearch = require("../module/filt_func");
+const Home = require("../models/home");
 
 const creatCount = (data, limit, index = 1, arr = []) => {
   if (data <= 0) {
@@ -27,10 +27,12 @@ router.post("/filtPage/:id", async (req, res) => {
     const filter = await createSbjectSearch(newBody);
     const countDocuments = await Home.find(filter).countDocuments();
     const count = creatCount(countDocuments, limit);
+
     const candidate = await Home.find(filter)
       .sort({ $natural: -1 })
       .skip((id.id - 1) * limit)
       .limit(limit);
+
     res.json({ candidate, count });
   } catch (e) {
     res.status(500).json({ message: "Something went wrong, please try again" });
