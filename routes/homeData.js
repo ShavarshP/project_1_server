e372cb0a048f74b5cec3bdfc457c012d823068a0
Lenovo <shavarsh101097@mail.ru>
2021-10-30 20:44:26 +0400

    add: delete func

--- routes/homeData.js
+++ routes/homeData.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const Home = require("../models/home");
 const HomeList = require("../models/homeList");
+const auth = require("../middleware/auth.middleware");
 
 router.get("/houses", async (req, res) => {
   const candidate = await Home.find().sort({ $natural: -1 }).limit(8);
@@ -15,6 +16,16 @@ router.get("/my_home/:_id", async (req, res) => {
     const img = await HomeList.findOne({ owner: id });
     homedate.img = img.img;
     res.json(homedate);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong, please try again" });
+  }
+});
+
+router.get("/delete/:_id", auth, async (req, res) => {
+  try {
+    const id = req.params;
+    await Home.deleteOne(id)
+    res.json({msj: 'delete data'});
   } catch (e) {
     res.status(500).json({ message: "Something went wrong, please try again" });
   }
