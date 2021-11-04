ee14060d20e6db6d1a4aab3b284092c21c66bab5
Lenovo <shavarsh101097@mail.ru>
2021-11-04 21:38:12 +0400

    add: img func

--- routes/setData.js
+++ routes/setData.js
@@ -56,4 +56,26 @@ router.post("/add", async (req, res) => {
   }
 });
 
+// router.get('/images/:key', (req, res) => {
+//   console.log(req.params)
+//   const key = req.params.key
+//   const readStream = getFileStream(key)
+
+//   readStream.pipe(res)
+// })
+
+// router.post('/images', upload.single('image'), async (req, res) => {
+//   const file = req.file
+//   // console.log(file)
+
+//   // apply filter
+//   // resize 
+
+//   const result = await uploadFile(file)
+//   await unlinkFile(file.path)
+//   // console.log(result)
+//   const description = req.body.description
+//   res.send({ imagePath: `/images/${result.Key}` })
+// })
+
 module.exports = router;
