eb700ff288c11d701f7dee7d3086c6592413b419
Lenovo <shavarsh101097@mail.ru>
2021-11-06 16:43:22 +0400

    add: updat api

--- routes/setData.js
+++ routes/setData.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const router = Router();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-const Home = require("../models/home");
-const HomeList = require("../models/homeList");
+// const Home = require("../models/home");
+// const HomeList = require("../models/homeList");
+
+// const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
 
 router.post("/add", async (req, res) => {
   try {
@@ -55,6 +59,53 @@ router.post("/add", async (req, res) => {
     res.status(500).json({ message: "Something went wrong, please try again" });
   }
 });
+
+router.post("/updat/:id", async (req, res) => {
+  try {
+    const {
+      sale,
+      rent,
+      search_code,
+      category,
+      district,
+      building_type,
+      rooms,
+      price,
+      floor,
+      area,
+      street,
+      Mobile_number,
+      description,
+      img,
+    } = req.body;
+    const id = req.params;
+
+    Home.updateOne(
+      { _id: id }, {
+      img: img[0],
+      Mobile_number: Mobile_number,
+      area: area,
+      building_type: building_type,
+      category: category,
+      street: street,
+      description: description,
+      district: district,
+      floor: floor,
+      price: price,
+      rent: rent,
+      rooms: rooms,
+      sale: sale,
+      search_code: search_code,
+      loc: "",
+    });
+
+    res.status(201).json({ message: "" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Something went wrong, please try again" });
+  }
+});
+
 
 // router.get('/images/:key', (req, res) => {
 //   console.log(req.params)
